c07143d2db083497bd5109579cd4987619160285
ARUN SINGH <arunsinghh0011@gmail.com>
2025-12-30 22:12:15 -0800

    all bugs fixed

--- ColorChanger/app.js
+++ ColorChanger/app.js
@@ -1,9 +1,13 @@
-const randomColor = Math.floor(Math.random() * 1111111);
+const mybtn = document.getElementById("btn");
+const myDiv = document.getElementById("wrapper-div");
+const heading = document.getElementById("heading");
 
 function changeBackgroundColor() {
-  document.getElementById("wrapper-div").style.backgroundColor =
-    "#" + randomColor;
-  document.getElementById("btn").style.backgroundColor = "#" + randomColor;
-  document.getElementById("heading").innerHTML =
-    "Background Color:" + " #" + randomColor;
+  const randomColor = "#" + Math.floor(Math.random() * 1111111);
+
+  myDiv.style.backgroundColor = randomColor;
+  mybtn.style.backgroundColor = randomColor;
+  heading.innerHTML = "Background Color: " + randomColor;
 }
+
+mybtn.addEventListener("click", changeBackgroundColor);
