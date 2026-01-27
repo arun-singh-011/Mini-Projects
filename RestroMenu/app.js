f8d1eb97204648591a8406d3724be9141b591d0a
ARUN SINGH <arunsinghh0011@gmail.com>
2026-01-26 21:45:54 -0800

    restro menu project in progress

--- RestroMenu/app.js
+++ RestroMenu/app.js
@@ -1,1 +1,14 @@
-const fil = document.querySelectorAll(".filters");
+// const fil = document.querySelectorAll(".filters");
+const all = document.getElementById("getAll");
+const breakfast = document.getElementById("getBreakfast");
+const lunch = document.getElementById("getLunch");
+const dinner = document.getElementById("getDinner");
+const shakes = document.getElementById("getShakes");
+
+breakfast.addEventListener("click", () => {
+  breakfast.style.display = "block";
+  lunch.style.display = "none";
+
+  dinner.style.display = "none";
+  shakes.style.display = "none";
+});
